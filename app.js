e23ac12d392867cc327e02a286849d83f83c0040
Darien <darienjensen@outlook.com>
2023-11-29 13:17:16 -0700

    drawTown()

---
+++ app.js
@@ -0,0 +1,74 @@
+const locations = [
+    '🏤', '🏥', '🏭', '🏢', '🏣'
+]
+
+const people = [{
+        name: 'Jimbo',
+        picture: '🤵',
+        location: '🏤'
+    },
+    {
+        name: 'Sammy',
+        picture: '🙆‍♀️',
+        location: '🏤'
+    },
+    {
+        name: 'Michael',
+        picture: '👷',
+        location: '🏤'
+    },
+    {
+        name: 'Robert',
+        picture: '👷',
+        location: '🏥'
+    },
+    {
+        name: 'Terry',
+        picture: '🤴',
+        location: '🏥',
+    },
+    {
+        name: 'Bill',
+        picture: '🕵️',
+        location: '🏥',
+    },
+    {
+        name: 'Marie',
+        picture: '👩‍🍳',
+        location: '🏭',
+    },
+    {
+        name: 'Mykeal',
+        picture: '💂',
+        location: '🏭',
+    },
+    {
+        name: 'Phil',
+        picture: '🧜‍♂️',
+        location: '🏭',
+    },
+    {
+        name: 'Wilson',
+        picture: '🏐',
+        location: '🏢',
+    },
+    {
+        name: 'Wendy',
+        picture: '👩‍⚕️',
+        location: '🏢',
+    },
+    {
+        name: 'Jeremy',
+        picture: '🦹',
+        location: '🏢',
+    }
+]
+
+function drawTown(){
+    locations.forEach(location =>{
+        let group = people.filter(person => person.location == location)
+        let groupPics = group.map(group => group.picture)
+        document.getElementById(location).innerText = groupPics.join(' ')
+    })
+}
+drawTown()
